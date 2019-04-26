c52530fe60e945ab5323029bb12a8c4f7c908d03
santiart <47418108+santiart@users.noreply.github.com>
2019-04-26 12:23:26 -0300

    Add files via upload

---
+++ arduinop/sketch_apr26a/sketch_apr26a.ino
@@ -0,0 +1,36 @@
+const int LED = 11;
+const int BOTON = 10;
+int val;
+int est;
+int valA;
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  pinMode(BOTON, INPUT);
+
+}
+
+void loop() {
+
+ Funcion(val,est,valA);
+
+}
+
+void Funcion (int valor, int estado, int valor_viejo )
+{
+  valor = 0;
+  valor_viejo = 0;
+  estado = 0;
+  valor = digitalRead(BOTON);
+
+  if ((valor == HIGH) && (valor_viejo == LOW))
+  {
+    estado = 1 - estado;
+    delay(10);
+  }
+  else
+  {
+    digitalWrite(LED, LOW);
+  }
+}
+
